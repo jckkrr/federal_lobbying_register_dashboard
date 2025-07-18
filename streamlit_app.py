b142cd7e27f8070582d791f2a1c1d3704322b7bf
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-07-18 12:52:03 +1000

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,37 +34,13 @@ def get_l2c():
     
     return df_l2c
 
-def search_l2c(chosen_search_column, chosen_search_term, only_exgov, only_energy, inc_public_servants):
-    
-    df_search = get_l2c()
-    
-    if chosen_search_column != None and chosen_search_term != None:
-        df_search = df_search.loc[df_search[chosen_search_column] == chosen_search_term] 
-    
-    if only_exgov == True:
-        df_search = df_search.loc[df_search['Former Govt. Representative'] == 'Yes']
-        
-    if only_energy == True:
-        df_search = df_search.loc[df_search['is_energy'] == 'Yes']
-        
-    if inc_public_servants == False:
-        exclude_positions = ['Employed under the Public Service Act 1999', 'Contractor/consultant for an agency whose staff are employed under the Public Service Act 1999', 'Public Servant']
-        df_search['Former Govt. Representative'] = np.where(df_search['Previous Position'].isin(exclude_positions), 'No', df_search['Former Govt. Representative'])
-            
-    df_search = df_search.sort_values(by = ['is_energy', 'Former Govt. Representative', 'Cessation Year', 'Lobbyist Name'], ascending = [True, False, False, True])
-    df_search = df_search[['Lobbyist Name', 'Job Title', 'Parent Organisation', 'Org ABN', 'Former Govt. Representative', 'Previous Position', 'Cessation Year', 'Client Name', 'Client ABN', 'is_energy']]
-    
-    df_search['Cessation Year'] = df_search['Cessation Year'].astype(float)
-    
-    st.dataframe(df_search.style.format(precision=0))
-
 
 ### ----------------------------------------  RUN
 
 st.markdown("**Open Investigation Tools** | [constituent.online](%s)" % 'http://www.constituent.online')
     
 st.title('Australian Federal Government Lobbying Register Dashboard')
-st.write('Data downloaded: 18 July 2025 | General public servants not counted as former government representatives')
+st.write('Data downloaded: 18 July 2025')
 
 ### Get DFs ### 
 
@@ -79,26 +55,46 @@ df_l2c = get_l2c()
 with col1: 
     available_search_column = [None, 'Lobbyist Name', 'Parent Organisation', 'Previous Position', 'Client Name'] 
     chosen_search_column = st.selectbox('Search column:', (available_search_column))
-
+    
 with col2: 
+    
     if chosen_search_column != None:
         available_search_terms = [None] + sorted(df_l2c[chosen_search_column].dropna().unique())
         chosen_search_term = st.selectbox('Search term:', (available_search_terms))
     else:
         chosen_search_term = None
         st.write('...')
+        
+    if chosen_search_column != None and chosen_search_term != None:
+        df_l2c = df_l2c.loc[df_l2c[chosen_search_column] == chosen_search_term] 
 
 with col3:
     available_only_exgov = [False, True]
     only_exgov = st.selectbox('Ex govt reps only', (available_only_exgov))
     
+    if only_exgov == True:
+        df_l2c = df_l2c.loc[df_l2c['Former Govt. Representative'] == 'Yes']
+    
 with col4:
     available_only_energy = [False, True]
     only_energy = st.selectbox('Enegry clients only', (available_only_energy))
     
+    if only_energy == True:
+        df_l2c = df_l2c.loc[df_l2c['is_energy'] == 'Yes']
+    
 with col5:
     available_inc_public_servants = [False, True]
     inc_public_servants = st.selectbox('Inc public servants as govt reps', (available_only_energy))
+    
+    if inc_public_servants == False:
+        exclude_positions = ['Employed under the Public Service Act 1999', 'Contractor/consultant for an agency whose staff are employed under the Public Service Act 1999', 'Public Servant']
+        df_l2c['Former Govt. Representative'] = np.where(df_l2c['Previous Position'].isin(exclude_positions), 'No', df_l2c['Former Govt. Representative'])
 
-search_l2c(chosen_search_column, chosen_search_term, only_exgov, only_energy, inc_public_servants)
+        
+df_l2c = df_l2c.sort_values(by = ['is_energy', 'Former Govt. Representative', 'Cessation Year', 'Lobbyist Name'], ascending = [True, False, False, True])
+df_l2c = df_l2c[['Lobbyist Name', 'Job Title', 'Parent Organisation', 'Org ABN', 'Former Govt. Representative', 'Previous Position', 'Cessation Year', 'Client Name', 'Client ABN', 'is_energy']]
+    
+df_l2c['Cessation Year'] = df_l2c['Cessation Year'].astype(float)
+    
+st.dataframe(df_l2c.style.format(precision=0)) 
         
