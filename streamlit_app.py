d4193c2af4f05eb1b86eaee86f8551685cd8c5d5
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2025-07-18 12:35:41 +1000

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,104 @@
+############## Constistuent.Online #################
+####### Code-free analysis for curious folk. ######
+
+### An application for ...
+
+## streamlit run "C:\Users\Jack\Documents\Python_projects\greenpeace\lobbyists\streamlit_app.py"
+
+### --------------------------------------- IMPORTS 
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+
+import customChartDefaultStyling
+
+pd.set_option('display.max_columns', None)
+
+### 
+headers = {
+    "content-type": "application/json"
+}
+
+css = 'body, html, p, h1, .st-emotion-cache-1104ytp h1, [class*="css"] {font-family: "Inter", sans-serif;}'
+st.set_page_config(layout="wide")
+st.markdown( f'<style>{css}</style>' , unsafe_allow_html= True)
+
+
+### ---------------------------------------- FUNCTIONS 
+
+### Get a person's comany and clients
+def get_l2c():
+    df_l2c = df_lobbyists.merge(df_clients[['Parent Organisation', 'Org ABN', 'Client Name', 'Client ABN', 'is_energy', 'Date Published']], on = ['Parent Organisation', 'Org ABN'], how = 'outer')
+    
+    return df_l2c
+
+def search_l2c(chosen_search_column, chosen_search_term, only_exgov, only_energy, inc_public_servants):
+    
+    df_search = get_l2c()
+    
+    if chosen_search_column != None and chosen_search_term != None:
+        df_search = df_search.loc[df_search[chosen_search_column] == chosen_search_term] 
+    
+    if only_exgov == True:
+        df_search = df_search.loc[df_search['Former Govt. Representative'] == 'Yes']
+        
+    if only_energy == True:
+        df_search = df_search.loc[df_search['is_energy'] == 'Yes']
+        
+    if inc_public_servants == False:
+        exclude_positions = ['Employed under the Public Service Act 1999', 'Contractor/consultant for an agency whose staff are employed under the Public Service Act 1999', 'Public Servant']
+        df_search['Former Govt. Representative'] = np.where(df_search['Previous Position'].isin(exclude_positions), 'No', df_search['Former Govt. Representative'])
+            
+    df_search = df_search.sort_values(by = ['is_energy', 'Former Govt. Representative', 'Cessation Year', 'Lobbyist Name'], ascending = [True, False, False, True])
+    df_search = df_search[['Lobbyist Name', 'Job Title', 'Parent Organisation', 'Org ABN', 'Former Govt. Representative', 'Previous Position', 'Cessation Year', 'Client Name', 'Client ABN', 'is_energy']]
+    
+    df_search['Cessation Year'] = df_search['Cessation Year'].astype(float)
+    
+    st.dataframe(df_search.style.format(precision=0))
+
+
+### ----------------------------------------  RUN
+
+st.markdown("**Open Investigation Tools** | [constituent.online](%s)" % 'http://www.constituent.online')
+    
+st.title('Australian Federal Government Lobbying Register Dashboard')
+st.write('Data downloaded: 18 July 2025 | General public servants not counted as former government representatives')
+
+### Get DFs ### 
+
+df_organisations = pd.read_csv('https://raw.githubusercontent.com/jckkrr/federal_lobbying_register_dashboard/refs/heads/main/df_organisations.csv')
+df_lobbyists = pd.read_csv('https://raw.githubusercontent.com/jckkrr/federal_lobbying_register_dashboard/refs/heads/main/df_lobbyists.csv')
+df_clients = pd.read_csv('https://raw.githubusercontent.com/jckkrr/federal_lobbying_register_dashboard/refs/heads/main/df_clients.csv')
+
+col1, col2, col3, col4, col5 = st.columns([1,2,1,1,1])
+
+df_l2c = get_l2c()
+
+with col1: 
+    available_search_column = [None, 'Lobbyist Name', 'Parent Organisation', 'Previous Position', 'Client Name'] 
+    chosen_search_column = st.selectbox('Search column:', (available_search_column))
+
+with col2: 
+    if chosen_search_column != None:
+        available_search_terms = [None] + sorted(df_l2c[chosen_search_column].dropna().unique())
+        chosen_search_term = st.selectbox('Search term:', (available_search_terms))
+    else:
+        chosen_search_term = None
+        st.write('...')
+
+with col3:
+    available_only_exgov = [False, True]
+    only_exgov = st.selectbox('Ex govt reps only', (available_only_exgov))
+    
+with col4:
+    available_only_energy = [False, True]
+    only_energy = st.selectbox('Enegry clients only', (available_only_energy))
+    
+with col5:
+    available_inc_public_servants = [False, True]
+    inc_public_servants = st.selectbox('Inc public servants as govt reps', (available_only_energy))
+
+search_l2c(chosen_search_column, chosen_search_term, only_exgov, only_energy, inc_public_servants)
+        
